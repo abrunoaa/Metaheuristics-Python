71f19e718bf0b6759f21b949ca6b1f0134fb2aca
jockey6 <abrunoaa@gmail.com>
2020-03-14 10:43:29 -0300

    .csv like output

--- run_tests.py
+++ run_tests.py
@@ -11,7 +11,7 @@ def run(filename, tests, function):
   total_elapsed = 0
   avg_answer = 0
   best, worst = float("inf"), 0
-  print("elapsed,result,tour")
+  print("elapsed;result;tour")
 
   for test in range(tests):
     start = time()
@@ -19,12 +19,13 @@ def run(filename, tests, function):
     end = time()
 
     elapsed = end - start
-    print("{:.3f}s,{},{}".format(elapsed, ans.get_fitness(), ans.get_tour()))
+    print("{:.3f}s;{};{}".format(elapsed, ans.get_fitness(), ans.get_tour()))
 
     total_elapsed += elapsed
     avg_answer += ans.get_fitness()
     best = min(best, ans.get_fitness())
     worst = max(worst, ans.get_fitness())
 
-  print("avg_elapsed,avg_result,best_result,worst_result")
-  print("{:.3f}s,{:.3f},{},{}".format(total_elapsed / tests, avg_answer / tests, best, worst))
+  print("")
+  print("avg_elapsed;avg_result;best_result;worst_result")
+  print("{:.3f}s;{:.3f};{};{}".format(total_elapsed / tests, avg_answer / tests, best, worst))
