993c372d324a8582fdca74224c912b595de51403
jockey6 <abrunoaa@gmail.com>
2020-03-14 10:43:50 -0300

    Better parameters

--- simulated_annealing_cvrp.py
+++ simulated_annealing_cvrp.py
@@ -5,13 +5,13 @@ from run_tests import run
 
 
 def function(cvrp):
-  max_temperature = 2 * cvrp.number_of_clients
+  max_temperature = 10
   min_temperature = 1
-  cooling_rate = .99
+  cooling_rate = .999
   return simulated_annealing(max_temperature, min_temperature, cooling_rate, Solution(cvrp))
 
 
 # debugging
 if __name__ == "__main__":
-  run('A\\A-n32-k5.vrp', 100, function)
-  # run('X\\X-n256-k16.vrp', 100, function)
+  # run('A\\A-n32-k5.vrp', 100, function)
+  run('X\\X-n256-k16.vrp', 100, function)
