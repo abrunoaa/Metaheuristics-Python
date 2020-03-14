86c92016a9382a571d09a7b2bc6c37ec54d07247
jockey6 <abrunoaa@gmail.com>
2020-03-14 10:44:17 -0300

    Cython source file

--- combinatorial/tsp/setup.py
+++ combinatorial/tsp/setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from os import path
 
+folder = path.dirname(__file__)
 setup(
-    ext_modules = cythonize("two_opt.pyx", compiler_directives = {'language_level': "3"})
+    ext_modules = cythonize(path.join(folder, "two_opt.pyx"), compiler_directives = {'language_level': "3"})
 )
--- combinatorial/tsp/two_opt.pyx
+++ combinatorial/tsp/two_opt.pyx
@@ -4,59 +4,67 @@ from libc.stdlib cimport malloc, free
 from logging import info
 
 
-cpdef two_opt(tour, distance):
-  info("Running 2-opt in C")
-
-  # validations
-  assert isinstance(tour, list), "'tour' must be a list"
-  assert callable(distance), "'distance' must be a function"
-  if tour is None or len(tour) < 2:
-    return 0, [0]
-  assert len(set(tour)) == len(tour), "Duplicates found in tour"
-  assert tour[-1] == 0, "Expected depot at end of tour, found {}".format(tour[-1])
-
+cdef long c_two_opt(unsigned n, unsigned* tour, distance):
   # declarations
-  cdef int n = len(tour)
-  cdef unsigned* c_tour = <unsigned*>malloc(len(tour) * cython.sizeof(int))
+  cdef unsigned* dist = <unsigned*>malloc(n * cython.sizeof(int))
 
   cdef long improve = 0
   cdef long best, reduce
   cdef unsigned left = 0, right = 0
   cdef unsigned a, b, c, d, tmp
 
-  # convert to C
-  for i in range(n):
-    c_tour[i] = tour[i]
-
-  # remaining code
+  # execution
   while True:
+    for i in range(n):
+      dist[i] = distance(tour[i - 1], tour[i])
     best = 0
     for i in range(-1, n - 3):
-      a = c_tour[i]
-      b = c_tour[i + 1]
+      a = tour[i]
+      b = tour[i + 1]
       for j in range(i + 2, n - 1):
-        c = c_tour[j]
-        d = c_tour[j + 1]
-        reduce = distance(a, b) + distance(c, d) - distance(a, c) - distance(b, d)
+        c = tour[j]
+        d = tour[j + 1]
+        reduce = dist[i + 1] + dist[j + 1] - distance(a, c) - distance(b, d)
         if reduce > best:
           best = reduce
           left = i + 1
           right = j
 
     if best == 0:
-      assert set(tour) == set([u for u in c_tour[: n]])
-      tour = [u for u in c_tour[: n]]
-      free(c_tour)
-      return int(improve), tour
+      free(dist)
+      return improve
 
     assert left < right
     improve += best
     while left < right:
-      tmp = c_tour[left]
-      c_tour[left] = c_tour[right]
-      c_tour[right] = tmp
+      tmp = tour[left]
+      tour[left] = tour[right]
+      tour[right] = tmp
       left += 1
       right -= 1
 
-    assert c_tour[-1] == 0
-    assert len(set([u for u in c_tour[: n]])) == n
+    assert tour[-1] == 0
+
+cpdef two_opt(tour, distance):
+  info("Running 2-opt in C")
+
+  # validations
+  assert isinstance(tour, list), "'tour' must be a list"
+  assert callable(distance), "'distance' must be a function"
+  if tour is None or len(tour) <= 2:
+    return 0, tour
+  assert len(set(tour)) == len(tour), "Duplicates found in tour"
+  assert tour[-1] == 0, "Expected depot at end of tour, found {}".format(tour[-1])
+
+  cdef unsigned n = len(tour)
+  cdef unsigned* c_tour = <unsigned*>malloc(n * cython.sizeof(int))
+  for i in range(n):
+    c_tour[i] = tour[i]
+
+  improve = c_two_opt(n, c_tour, distance)
+
+  for i in range(n):
+    tour[i] = c_tour[i]
+
+  free(c_tour)
+  return improve, tour
