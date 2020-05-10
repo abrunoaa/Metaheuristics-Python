60ce46106a6a2eb385faac1c6bba6bc9499bc934
jockey6 <abrunoaa@gmail.com>
2020-05-10 17:26:46 -0300

    add possibility to return to node 0 when contructing path

--- combinatorial/cvrp/cvrp_ant.py
+++ combinatorial/cvrp/cvrp_ant.py
@@ -37,22 +37,24 @@ class CvrpAnt(Ant, CvrpSolution):
 
   def travel(self, alpha, beta, pheromone, quality):
     n = len(pheromone) - 1
-    candidate = [u for u in range(1, n + 1)]
 
     assert len(self.tour) == n, "Nodes between tour and pheromone: {} != {}".format(len(self.tour), n)
 
     u = 0
     load = 0
-    for i in range(0, len(candidate)):
+    candidate = [u for u in range(0, n + 1)]
+    for i in range(0, len(candidate) - 1):
+      assert candidate[0] == 0, "Implementation error: changed candidate 0 to {}".format(candidate[0])
+
       # FIXME:
       #  Why the solution is better for higher values of alpha and beta?
       #  Because the variables are small and becomes 0?
       probability = [pheromone[u][v] ** alpha * quality[u][v] ** beta for v in candidate]
       u = candidate[roulette(x for x in probability)]
 
-      if load + self.cvrp.get_demand(u) > self.cvrp.capacity:
-        probability = [pheromone[0][v] ** alpha * quality[0][v] ** beta for v in candidate]
-        u = candidate[roulette(x for x in probability)]
+      if u == 0 or load + self.cvrp.get_demand(u) > self.cvrp.capacity:
+        probability = [pheromone[0][v] ** alpha * quality[0][v] ** beta for v in candidate[1:]]
+        u = candidate[roulette(x for x in probability) + 1]
         load = 0
 
       load += self.cvrp.get_demand(u)
