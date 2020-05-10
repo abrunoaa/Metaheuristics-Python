87ec8c8ce3e3409d1f918a1c5dab3429e8c88062
jockey6 <abrunoaa@gmail.com>
2020-05-10 16:44:28 -0300

    add CVRP with ACO test

--- run.sh
+++ run.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 results="results/sa/"
+
+# TSP
 for file in $(find . -name '*a280.tsp'); do
-#for file in $(find . -name '*X\\X-n101-k25.vrp'); do
+#  break
+  echo "Processing $file"
+#  pypy3 tsp_aco.py "$file"
+#  pypy3 tsp_ga.py "$file"
+#  pypy3 tsp_sa.py "$file"
+done
+
+# CVRP
+for file in $(find . -name '*X\\X-n101-k25.vrp'); do
+#  break
   output="${file%.*}.out"
   output="${output:12}"
   dir=$(dirname "${output}")
   result_file="$results$output"
   if [ ! -f "$result_file" ]; then
-    echo "Processing $result_file"
+    echo "Processing $file"
     mkdir -p "$results$dir"
-#    pypy3 tsp_sa.py "$file" # >"$result_file"
-#    pypy3 tsp_ga.py "$file" # >"$result_file"
-    pypy3 tsp_aco.py "$file" # >"$result_file"
-#    pypy3 cvrp_sa.py "$file" # >"$result_file"
+
+#    pypy3 cvrp_aco.py "$file" # >"$result_file"
 #    pypy3 cvrp_ga.py "$file" # >"$result_file"
+    pypy3 cvrp_sa.py "$file" # >"$result_file"
   else
-    echo "Ignoring $result_file"
+    echo "Ignoring $file"
   fi
 done
