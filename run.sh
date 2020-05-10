e87bc3af7c7727bddc3d3bf666fd239ba6fd3dc1
jockey6 <abrunoaa@gmail.com>
2020-05-10 14:05:24 -0300

    change script

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# CVRP
 results="results/sa/"
 for file in $(find . -name '*a280.tsp'); do
 #for file in $(find . -name '*X\\X-n101-k25.vrp'); do
@@ -11,11 +10,11 @@ for file in $(find . -name '*a280.tsp'); do
   if [ ! -f "$result_file" ]; then
     echo "Processing $result_file"
     mkdir -p "$results$dir"
-#    pypy3 sa_tsp.py "$file" # >"$result_file"
-#    pypy3 ga_tsp.py "$file" # >"$result_file"
-    pypy3 aco_tsp.py "$file" # >"$result_file"
-#    pypy3 sa_cvrp.py "$file" # >"$result_file"
-#    pypy3 ga_cvrp.py "$file" # >"$result_file"
+#    pypy3 tsp_sa.py "$file" # >"$result_file"
+#    pypy3 tsp_ga.py "$file" # >"$result_file"
+    pypy3 tsp_aco.py "$file" # >"$result_file"
+#    pypy3 cvrp_sa.py "$file" # >"$result_file"
+#    pypy3 cvrp_ga.py "$file" # >"$result_file"
   else
     echo "Ignoring $result_file"
   fi
