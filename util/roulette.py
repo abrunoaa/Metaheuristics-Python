37a7db1251bff1e69d14c52a9be28b91b82d2f0a
jockey6 <abrunoaa@gmail.com>
2020-05-31 12:10:31 -0300

    optimized roulette function

--- util/roulette.py
+++ util/roulette.py
@@ -14,26 +14,29 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <https://www.gnu.org/licenses/>.
-
+#
 from collections import Generator
-from random import uniform
+from random import random
 
 
-def roulette(values):
+def roulette(values, s=None):
   """
   Select one index from values.
 
-  The probability to choose index k is 1 / values[k].
+  The probability to choose index k is proportional to values[k].
 
-  :param values: List with probabilities to choose index k
+  :param values: List with values corresponding to the probability to choose index k
+  :param s: Sum of values. This is util, for example, if the sum always equals to 1.
   :return: An integer in range [0, len(values)), the chosen index
   """
-  if isinstance(values, Generator):
-    values = list(values)
-  r = uniform(0, sum(values))
-  s = 0
-  for i, x in enumerate(values):
-    s += x
-    if s > r:
+  if s is None:
+    if isinstance(values, Generator):
+      values = list(values)
+    s = sum(values)
+
+  r = random()
+  for i, x in enumerate(v / s for v in values):
+    r -= x
+    if r < 0:
       return i
   return len(values) - 1
